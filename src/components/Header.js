ad0a6d9a98d11380a5779b2ae64ada6727107083
Thiago Lira <thiago.slira@outlook.com>
2022-11-03 20:54:10 -0300

    Requisito 4 concluído

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Carregando from '../pages/Carregando';
 
@@ -28,6 +29,11 @@ class Header extends Component {
       <header data-testid="header-component">
         {isLoading ? <Carregando /> : (
           <span data-testid="header-user-name">{ user.name }</span>) }
+        <nav>
+          <Link to="/search" data-testid="link-to-search">Search</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+          <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+        </nav>
       </header>
     );
   }
