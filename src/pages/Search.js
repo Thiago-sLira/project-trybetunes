f4c7f29bb76c8160edda4185e440af4a7fa36a1c
Thiago Lira <thiago.slira@outlook.com>
2022-11-04 18:21:37 -0300

    Requisito 6 concluído

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Carregando from './Carregando';
 
 class Search extends Component {
   state = {
     nameArtist: '',
+    nameArtistShow: '',
     isLoginButtonDisabled: true,
+    isLoading: false,
+    isForm: true,
+    isArtist: false,
+    artist: [],
   };
 
   enableSearchButton = () => {
@@ -24,33 +32,79 @@ class Search extends Component {
     }, this.enableSearchButton);
   };
 
+  hadleButtonClick = async () => {
+    const { nameArtist } = this.state;
+    this.setState({
+      isLoginButtonDisabled: true,
+      isForm: false,
+      isLoading: true,
+      isArtist: true,
+      nameArtistShow: nameArtist,
+      nameArtist: '',
+    });
+    const result = await searchAlbumsAPI(nameArtist);
+    console.log(result);
+    this.setState({ isLoading: false, artist: result, isForm: true });
+  };
+
   render() {
-    const { nameArtist, isLoginButtonDisabled } = this.state;
+    const {
+      nameArtist, isForm, isArtist, artist, nameArtistShow,
+      isLoginButtonDisabled, isLoading,
+    } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <form>
-          <fieldset>
-            <legend>Pesquisa</legend>
-            <label htmlFor="input-artists">
-              <input
-                id="input-artists"
-                type="text"
-                data-testid="search-artist-input"
-                name="nameArtist"
-                value={ nameArtist }
-                onChange={ this.handleChange }
-              />
-            </label>
-            <button
-              type="button"
-              data-testid="search-artist-button"
-              disabled={ isLoginButtonDisabled }
-            >
-              Pesquisar
-            </button>
-          </fieldset>
-        </form>
+        {isForm && (
+          <form onSubmit={ this.hadleButtonClick }>
+            <fieldset>
+              <legend>Pesquisa</legend>
+              <label htmlFor="input-artists">
+                <input
+                  id="input-artists"
+                  type="text"
+                  data-testid="search-artist-input"
+                  name="nameArtist"
+                  value={ nameArtist }
+                  onChange={ this.handleChange }
+                />
+              </label>
+              <button
+                type="submit"
+                data-testid="search-artist-button"
+                disabled={ isLoginButtonDisabled }
+              >
+                Pesquisar
+              </button>
+            </fieldset>
+          </form>)}
+        {isLoading && <Carregando />}
+        {isArtist && (
+          <h4>
+            Resultado de álbuns de:
+            {' '}
+            {nameArtistShow}
+          </h4>
+        )}
+        {artist.length === 0 ? <h3>Nenhum álbum foi encontrado</h3> : (
+          <div>
+            <ul>
+              {artist.map((album) => (
+                <li key={ album.collectionId }>
+                  <img src={ album.artworkUrl100 } alt={ album.collectionName } />
+                  <h5>{ album.collectionName }</h5>
+                  <p>{ album.artistName }</p>
+                  <Link
+                    to={ `/album/${album.collectionId}` }
+                    data-testid={ `link-to-album-${album.collectionId}` }
+                  >
+                    Album
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     );
   }
