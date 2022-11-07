b2e18457d1fe651c43c373fbc12c187fff682020
Thiago Lira <thiago.slira@outlook.com>
2022-11-07 17:04:23 -0300

    Criado componente MusicCardFavorite

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import MusicCardFavorite from '../components/MusicCardFavorite';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Carregando from './Carregando';
 
 class Favorites extends Component {
+  state = {
+    isLoading: false,
+    favoriteSong: [],
+  };
+
+  componentDidMount() {
+    this.handleFavoriteSongs();
+  }
+
+  handleFavoriteSongs = async () => {
+    const resultFavorites = await getFavoriteSongs();
+    this.setState({ favoriteSong: resultFavorites });
+  };
+
   render() {
+    const { favoriteSong, isLoading } = this.state;
     return (
       <div data-testid="page-favorites">
         <Header />
         Favorites
+        {isLoading ? <Carregando /> : (
+          <MusicCardFavorite favoriteSong={ favoriteSong } />
+        )}
       </div>
     );
   }
